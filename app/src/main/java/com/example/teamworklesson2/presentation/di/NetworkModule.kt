897cba89d8431174031ba9bdffeda454ecd57941
Renakos <renakos0129@gmail.com>
2024-04-23 18:18:16 +0600

    Доработал модуль и добавил Interceptor

--- app/src/main/java/com/example/teamworklesson2/presentation/di/NetworkModule.kt
+++ app/src/main/java/com/example/teamworklesson2/presentation/di/NetworkModule.kt
@@ -1,12 +1,12 @@
 package com.example.teamworklesson2.presentation.di
 
 import com.example.teamworklesson2.presentation.data.remote.apiservice.Android15ApiService
+import com.example.teamworklesson2.presentation.data.remote.interceptor.KeyInterceptor
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
@@ -21,24 +21,15 @@ object NetworkModule {
     @Provides
     @Singleton
     fun provideOkHttpClient(): OkHttpClient = OkHttpClient().newBuilder()
-        .addInterceptor(
-            HttpLoggingInterceptor().setLevel(
-                HttpLoggingInterceptor.Level.BODY
-            )
-        )
-        .connectTimeout(60L, TimeUnit.SECONDS)
-        .readTimeout(60L, TimeUnit.SECONDS)
-        .writeTimeout(60L, TimeUnit.SECONDS)
-        .callTimeout(60L, TimeUnit.SECONDS)
-        .build()
+        .addInterceptor(KeyInterceptor("fc77e974d6msh784b7e904a8c866p1b1141jsncb8c14deddb4"))
+        .connectTimeout(60L, TimeUnit.SECONDS).readTimeout(60L, TimeUnit.SECONDS)
+        .writeTimeout(60L, TimeUnit.SECONDS).callTimeout(60L, TimeUnit.SECONDS).build()
 
     @Provides
     @Singleton
-    fun retrofitClient(okHttpClient: OkHttpClient): Retrofit = Retrofit.Builder()
-        .baseUrl(BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .client(okHttpClient)
-        .build()
+    fun retrofitClient(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient).build()
 
     @Provides
     @Singleton
