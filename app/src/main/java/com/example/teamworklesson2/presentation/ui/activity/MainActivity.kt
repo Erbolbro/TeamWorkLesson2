d56d034ba3872af323d5eabb76d4bca979d7afc8
Renakos <renakos0129@gmail.com>
2024-04-23 20:06:22 +0600

    Исправил все ошибки

--- app/src/main/java/com/example/teamworklesson2/presentation/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/teamworklesson2/presentation/ui/activity/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.example.teamworklesson2.presentation.ui.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.teamworklesson2.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -18,6 +20,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        viewModel
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        lifecycleScope.launch {
+            viewModel.fetchAndroid15().collect {
+                Log.e("correct", it.toString())
+            }
+        }
+
     }
 }
