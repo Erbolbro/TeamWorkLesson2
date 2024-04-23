0a1fee9b72eeb9ff054f68e7fe6209b09ce9af6a
pubgkonfuz <mashanloskm@gmail.com>
2024-04-23 16:17:48 +0600

    Я стянул все эти апишки

---
+++ app/src/main/java/com/example/teamworklesson2/data/model/Author.kt
@@ -0,0 +1,8 @@
+package com.example.teamworklesson2.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Author(
+    @SerializedName("author")
+    val author: String
+)
---
+++ app/src/main/java/com/example/teamworklesson2/data/model/Quote.kt
@@ -0,0 +1,8 @@
+package com.example.teamworklesson2.data.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Quote(
+    @SerializedName("quote")
+    val quote: String
+)
